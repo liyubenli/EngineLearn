b6db4a9fe3737c5d386491d2b8a7ab034baa4b44
liyubenli <liyubenli@outlook.com>
2023-08-29 18:58:04 +0800

    --triangle

---
+++ Project/OpenGL/Triangle.cpp
@@ -0,0 +1,207 @@
+#include "triangle.h"
+
+/*请确认是在包含GLFW的头文件之前包含了GLAD的头文件。
+GLAD的头文件包含了正确的OpenGL头文件（例如GL/gl.h），所以需要在其它依赖于OpenGL的头文件之前包含GLAD。*/
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+using namespace std;
+
+//三角形顶点
+float vertices[] =
+{
+    0.5f, 0.5f, 0.0f,   // 右上角
+    0.5f, -0.5f, 0.0f,  // 右下角
+    -0.5f, -0.5f, 0.0f, // 左下角
+    -0.5f, 0.5f, 0.0f   // 左上角
+};
+unsigned int indices[] = {
+    // 注意索引从0开始! 
+    // 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+    // 这样可以由下标代表顶点组合成矩形
+
+    0, 1, 3, // 第一个三角形
+    1, 2, 3  // 第二个三角形
+};
+//临时顶点着色器
+const char* m_vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+//临时片段着色器
+const char* m_fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
+//尺寸变化回调
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+//输入处理
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+//编译shader
+bool CompileShader(unsigned int shader, const char* vertexShaderSource)
+{
+    int  success;
+    char infoLog[512];
+
+    /*glShaderSource函数把要编译的着色器对象作为第一个参数。
+    第二参数指定了传递的源码字符串数量，这里只有一个。
+    第三个参数是顶点着色器真正的源码，
+    第四个参数我们先设置为NULL。*/
+    glShaderSource(shader, 1, &vertexShaderSource, NULL);
+
+    //尝试编译
+    glCompileShader(shader);
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int Triangle()
+{
+#pragma region GLFW 初始化以及设置
+
+    //初始化GLFW
+    glfwInit();
+
+    /*使用glfwWindowHint函数来配置GLFW。
+    glfwWindowHint函数的第一个参数代表选项的名称，我们可以从很多以GLFW_开头的枚举值中选择；
+    第二个参数接受一个整型，用来设置这个选项的值。
+    该函数参数参考：https://www.glfw.org/docs/latest/window.html#window_hints*/
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#pragma endregion
+
+#pragma region 创建窗口
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    //窗口尺寸发生变化回调
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+#pragma endregion
+
+#pragma region 渲染绘制
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    //顶点缓冲对象(Vertex Buffer Objects)
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+    /*glBufferData是一个专门用来把用户定义的数据复制到当前绑定缓冲的函数。
+    它的第一个参数是目标缓冲的类型：顶点缓冲对象当前绑定到GL_ARRAY_BUFFER目标上。
+    第二个参数指定传输数据的大小(以字节为单位)；用一个简单的sizeof计算出顶点数据大小就行。
+    第三个参数是我们希望发送的实际数据。
+    第四个参数指定了我们希望显卡如何管理给定的数据。它有三种形式：
+    GL_STATIC_DRAW ：数据不会或几乎不会改变。
+    GL_DYNAMIC_DRAW：数据会被改变很多。
+    GL_STREAM_DRAW ：数据每次绘制时都会改变。
+    三角形的位置数据不会改变，每次渲染调用时都保持原样，所以它的使用类型最好是GL_STATIC_DRAW。
+    如果，比如说一个缓冲中的数据将频繁被改变，那么使用的类型就是GL_DYNAMIC_DRAW或GL_STREAM_DRAW，这样就能确保显卡把数据放在能够高速写入的内存部分。*/
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    /*glVertexAttribPointer函数的参数非常多，所以我会逐一介绍它们：
+    第一个参数指定我们要配置的顶点属性。
+    第二个参数指定顶点属性的大小。顶点属性是一个vec3，它由3个值组成，所以大小是3。
+    第三个参数指定数据的类型，这里是GL_FLOAT(GLSL中vec*都是由浮点数值组成的)。
+    下个参数定义我们是否希望数据被标准化(Normalize)。如果我们设置为GL_TRUE，所有数据都会被映射到0（对于有符号型signed数据是-1）到1之间。我们把它设置为GL_FALSE。
+    第五个参数叫做步长(Stride)，这个参数的意思简单说就是从这个属性第二次出现的地方到整个数组0位置之间有多少字节。
+    最后一个参数的类型是void*，所以需要我们进行这个奇怪的强制类型转换。它表示位置数据在缓冲中起始位置的偏移量(Offset)。由于位置数据在数组的开头，所以这里是0。*/
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    //顶点着色器
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    CompileShader(vertexShader, m_vertexShaderSource);
+    //片段着色器
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    CompileShader(fragmentShader, m_fragmentShaderSource);
+    //着色器
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    //渲染循环
+    while (!glfwWindowShouldClose(window))
+    {
+        //输入
+        processInput(window);
+
+        //绘制物体
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        //清屏
+        /*glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);*/
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+#pragma endregion
+
+
+    return 0;
+}
--- Project/OpenGL/main.cpp
+++ Project/OpenGL/main.cpp
@@ -1,22 +1,7 @@
-
-//请确认是在包含GLFW的头文件之前包含了GLAD的头文件。
-//GLAD的头文件包含了正确的OpenGL头文件（例如GL/gl.h），所以需要在其它依赖于OpenGL的头文件之前包含GLAD。
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "Triangle.h"
 
 int main(void)
 {
-    //初始化GLFW
-    glfwInit();
-
-    //使用glfwWindowHint函数来配置GLFW。
-    //glfwWindowHint函数的第一个参数代表选项的名称，我们可以从很多以GLFW_开头的枚举值中选择；
-    //第二个参数接受一个整型，用来设置这个选项的值。
-    //该函数参数参考：https://www.glfw.org/docs/latest/window.html#window_hints
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    return 0;
-    return 0;
+	//三角形
+	Triangle();
 }
